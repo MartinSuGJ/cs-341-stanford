d6c530b913e842868db12d0f5e18f20bce2f0b9e
fannyfanyang <yangf.fanny@gmail.com>
2017-05-27 23:02:05 -0700

    friend list handling

---
+++ code/Node2vecWeight.py
@@ -0,0 +1,90 @@
+import argparse
+from scipy import spatial
+
+
+
+def parse_args():
+
+	parser = argparse.ArgumentParser(description="Run node2vec weight.")
+	# parser.add_argument('--edge', nargs='?', default='graph/karate.edgelist',
+	# 					help='Input graph path')
+
+	parser.add_argument('--fl', nargs='?', default='data/yelp_user_friends_list_train.edgelist',
+						help='Input graph path')	
+
+	parser.add_argument('--emb', nargs='?', default='data/yelp_user_friends_list_train.emb',
+						help='Embeddings path')
+
+	parser.add_argument('--weight', nargs='?', default='data/yelp_user_friends_list_train.weight',
+						help='Edge weight')
+	return parser.parse_args()
+
+if __name__ == '__main__':
+	args = parse_args()
+	# print(args)
+	# edge = args.edge
+	friendList = args.fl
+	emb = args.emb
+	weight = args.weight
+
+	nodeEmb = {}
+	embReader = open(emb, 'r')
+	skipLine = embReader.readline()
+	for line in embReader:
+		line = line.replace('\n','').split(' ')
+		nodeEmb[line[0]] = [float(line[i]) for i in range(1, len(line))]
+
+	embReader.close()
+
+	# edgeReader = open(edge, 'r')
+	# weightWriter = open(weight, 'w')
+	# for line in edgeReader:
+	# 	lineList = line.replace("\n","").split(" ")
+
+	# 	score = 1 - spatial.distance.cosine(nodeEmb[lineList[0]], nodeEmb[lineList[1]])
+	# 	weightWriter.write(line + ' ' + str(score) + '\n')
+
+	# edgeReader.close()
+	# weightWriter.close()
+
+	friendListReader = open(friendList, 'r')
+	weightWriter = open(weight, 'w')
+	skipLine = friendListReader.readline()
+	for line in friendListReader:
+		line = line.replace("\n","").split(",")
+		userID = line[0]
+		numFriend = int(line[1])
+		if numFriend == 0:
+			weightWriter.write(userID + ',' + str(0) + ',' + '\n')
+			continue
+		if userID not in nodeEmb:
+			weight = [str(1.0/numFriend)]*numFriend
+			weightWriter.write(userID + ',' + str(numFriend) + ',' + '::'.join(weight) + '\n')
+			continue
+		friendList = line[2].split("::")
+		weight = []
+		scoreSum = 0
+		for friend in friendList:
+			if friend in nodeEmb:
+				score = 1 - spatial.distance.cosine(nodeEmb[userID], nodeEmb[friend])
+				scoreSum += score
+				weight.append(str(score))
+			else:
+				weight.append[None]
+		avg = str(scoreSum/numFriend)
+		for i in range(numFriend):
+			if weight[i] == None:
+				weight[i] = avg
+			else:
+				weight[i] = str(float(weight[i])/numFriend)
+			weightWriter.write(userID + ',' + str(numFriend) + ',' + '::'.join(weight) + '\n')
+	friendListReader.close()
+	weightWriter.close()
+
+
+
+
+
+
+
+
---
+++ code/run.py
@@ -0,0 +1,57 @@
+import time
+import os
+import argparse
+from MatrixFactorization import MatrixFactorization
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("outputDirName", help="name of the output file")
+    parser.add_argument("predictType", help="type of the prediction. 1:predict all; 2:improve coverage rate; 3:ignore 234", type=int)
+    parser.add_argument("numIterList", help="parameter of number of iteration")
+    parser.add_argument("etaList", help="parameter of step size")
+    parser.add_argument("numLFList", help="parameter of dimension of latent factor")
+    parser.add_argument("lambdaList", help="parameter of regularization term")
+
+    args = parser.parse_args()
+    outputDirName = args.outputDirName
+    predictType = args.predictType
+    numIterList = [int(i) for i in args.numIterList.split("_")]
+    etaList = [float(i) for i in args.etaList.split("_")]
+    numLFList = [int(i) for i in args.numLFList.split("_")]
+    lambdaList = [float(i) for i in args.lambdaList.split("_")]
+
+    # parentDir = "/home/guangjun/"
+    parentDir = "/Users/FYang/Documents/A_Study/D_Courses/C_2017Spr/CS341/tune_params/Yelp/"
+    outputFile = parentDir + "result/" + outputDirName
+    if not os.path.exists(outputFile):
+        os.makedirs(outputFile)
+    trainFile = parentDir + "data/train.csv"
+    validateFile = parentDir + "data/validate.csv"
+    testFile = parentDir + "data/test.csv"
+
+    with open(outputFile+"/final_result_"+time.strftime("%Y%m%d_%H%M%S")+".txt", "w") as resultFile:
+        # Grid search step
+        print("====================Grid Search Step====================")
+        mf1 = MatrixFactorization(outputFile,trainFile, validateFile, 720165, 48059, predictType)
+        # numIterList = [40]
+        # etaList = [10e-2,10e-3]
+        # numLFList = [4,6]
+        # lambdaList = [0.2,0.3]
+        bestModel = mf1.gridSeacrh(numIterList = numIterList, etaList = etaList, numLFList = numLFList, lambdaList = lambdaList)
+        bestModelString = ("Best model is eta = %f, rank = %d, lambda = %f, num of iteration = %d\n" % (bestModel.bestEta, bestModel.bestNumLF, bestModel.bestLambda, bestModel.bestNumIter))
+        print(bestModelString)
+        resultFile.write(bestModelString)
+
+        # compute the remse error of the test set
+        print("====================Test Step====================")
+
+        mf2 = MatrixFactorization(outputFile,trainFile, testFile, 720165, 48059, predictType)
+        (rmseAll1,rmseAll2,rmsePart) = mf2.getRMSE(bestModel)
+        rmseAll1String = ("RMSE(All1) = %f for the model trained with eta = %f, rank = %d, lambda = %f, num of iteration = %d\n"  % (rmseAll1, bestModel.bestEta, bestModel.bestNumLF, bestModel.bestLambda, bestModel.bestNumIter))
+        rmseAll2String = ("RMSE(All2) = %f for the model trained with eta = %f, rank = %d, lambda = %f, num of iteration = %d\n"  % (rmseAll2, bestModel.bestEta, bestModel.bestNumLF, bestModel.bestLambda, bestModel.bestNumIter))
+        rmsePartString = ("RMSE(Part) = %f for the model trained with eta = %f, rank = %d, lambda = %f, num of iteration = %d\n"  % (rmsePart, bestModel.bestEta, bestModel.bestNumLF, bestModel.bestLambda, bestModel.bestNumIter))
+        resultFile.write(rmseAll1String)
+        resultFile.write(rmseAll2String)
+        resultFile.write(rmsePartString)
