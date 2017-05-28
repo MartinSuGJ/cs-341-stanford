8950eb66ec9cdfd23c6f71088576e577647ac29c
fannyfanyang <yangf.fanny@gmail.com>
2017-05-28 01:21:29 -0700

    modify friend review percentage

--- code/analysis/friendReviewPercent.py
+++ code/analysis/friendReviewPercent.py
@@ -4,14 +4,14 @@ import argparse
 
 def parse_args():
 
-	parser = argparse.ArgumentParser(Description='Calculate valid friend review percentage')
+	parser = argparse.ArgumentParser(description='Calculate valid friend review percentage')
 
 	
 	parser.add_argument('--trainAndVal', nargs='?', default='data/trainAndValidate.csv')
 	parser.add_argument('--test', nargs='?', default='data/test.csv')
 	parser.add_argument('--friendList', nargs='?', default='data/yelp_user_friends_list_trainAndValidate.csv')
 	parser.add_argument('--output', nargs='?', default='data/test_reviewPecent.csv')
-
+	return parser.parse_args()
 
 
 if __name__ == '__main__':
@@ -35,7 +35,7 @@ if __name__ == '__main__':
 	friendListReader = open(friendList, 'r')
 	skipLine = friendListReader.readline()
 	for line in friendListReader:
-		line = line.replace('\n', '')split(',')
+		line = line.replace('\n', '').split(',')
 		if int(line[1]) == 0:
 			continue
 		friends[line[0]] = line[2].split('::')
@@ -50,14 +50,14 @@ if __name__ == '__main__':
 		user_id = line[0]
 		business_id = line[1]
 		if user_id not in friends:
-			outputWriter.write(user_id + ',' + business_id + str(0))
+			outputWriter.write(user_id + ',' + business_id + ',' + str(0) + '\n')
 			continue
 		count = 0.0
 		for friend in friends[user_id]:
 			if (friend, business_id) in reviews:
 				count += 1
 		percentage = count/len(friends[user_id])
-		outputWriter.write(user_id + ',' + business_id + str(percentage))
+		outputWriter.write(user_id + ',' + business_id + ',' + str(percentage) + '\n')
 
 
 
